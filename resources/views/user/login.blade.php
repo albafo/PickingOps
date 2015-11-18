11df46a0aea87395ba017bd5a7894dc85dc42790
Álvaro Baño Fos <alvarobanofos@gmail.com>
2015-11-18 12:05:42 +0100

    First commit

---
+++ resources/views/user/login.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/custom.css')}}" rel="stylesheet">
+
+    <title>PickingOps - v1.01</title>
+</head>
+<body>
+    <div class="container">
+
+        <div class="starter-template">
+            <h1>Logival PickingOps v1.01</h1>
+        </div>
+
+    </div><!-- /.container -->
+    <script src="{{asset('js/jquery.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+</body>
+</html>
